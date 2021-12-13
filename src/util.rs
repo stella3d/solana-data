55369965f701f842ee44ef43d1c1dfc3bd37e874
Stella Cannefax <stella.cannefax+git@gmail.com>
2021-12-12 17:41:59 -0800

    comments / spacing in utils

--- src/util.rs
+++ src/util.rs
@@ -12,12 +12,13 @@ pub fn loop_task<F: Fn() -> ()>(total_time: Duration, loop_fn: F) {
     println!("loop task finished after {:3} seconds", start.elapsed().as_secs_f32());
 }
 
+
+// EXECUTION TIMING
 pub struct TimedData<T> {
     pub time: Duration,
     pub data: T
 }
 
-// helper for timing code execution
 pub fn timer<T: FnOnce() -> ()>(func: T) -> Duration {
     let start = Instant::now();
     func();
@@ -34,10 +35,13 @@ pub(crate) fn minutes_duration(minutes: u64) -> Duration {
     Duration::from_secs(60 * minutes)
 }
 
+
+// LOGGING (errors)
 pub fn log_err<E: Debug + Display>(e: &E) { eprintln!("{}", e); }
 
 pub fn log_err_none<T, E: Debug + Display>(e: E) -> Option<T> { eprintln!("{}", e); None }
 
+// LOGGING (slices)
 pub(crate) fn println_each_indent<T: Display>(data: &[T], end_newline: bool) {
     data.iter().for_each(|d| println!("    {}", d));
     if end_newline { println!("") }
